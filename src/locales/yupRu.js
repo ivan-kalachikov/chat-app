7b10ac3e4ba77b3f7ef98db79c858dd582f940fd
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2021-06-03 13:36:14 +0700

    step 8

--- src/locales/yupRu.js
+++ src/locales/yupRu.js
@@ -4,12 +4,12 @@ const buildYupLocale = (lng, t) => {
   Yup.setLocale({
     mixed: {
       required: t('errors.required'),
+      oneOf: t('errors.equalPasswords'),
       notOneOf: t('errors.shouldBeUnique'),
     },
     string: {
       min: ({ min }) => t('errors.min', { count: min }),
       max: ({ max }) => t('errors.max', { count: max }),
-      oneOf: t('errors.equalPasswords'),
     },
   });
 };

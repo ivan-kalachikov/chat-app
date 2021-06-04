f739f890c188f24da3bdc1bf3cacad9f2794b62b
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2021-06-04 14:55:14 +0700

    refactor

--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ import init from './init.jsx';
 
 import '../assets/application.scss';
 
-// eslint-disable-next-line no-unused-vars
-const rollbar = new Rollbar({
-  accessToken: '58f78fa326fd4754afcd5e6e7186e9c7',
-  captureUncaught: true,
-  captureUnhandledRejections: true,
-});
-
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
+} else {
+// eslint-disable-next-line no-unused-vars
+  const rollbar = new Rollbar({
+    accessToken: '58f78fa326fd4754afcd5e6e7186e9c7',
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+  });
 }
 
 init();

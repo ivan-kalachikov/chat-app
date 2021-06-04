afa6c3ba6d717f97db17976e6e7ac39656bcb264
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2021-06-04 18:40:46 +0700

    refactor

--- src/initApp.jsx
+++ src/initApp.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import App from './App.jsx';
 
-const initApp = (socketClient) => Promise.resolve(<App socketClient={socketClient} />);
+const initApp = (socket) => Promise.resolve(<App socket={socket} />);
 
 export default initApp;

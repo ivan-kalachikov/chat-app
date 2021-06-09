8a3b8afb0cb1aafab00e51ebd80816658c1b1d3c
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2021-06-09 21:38:33 +0700

    remove Slice from slice names

---
+++ src/slices/messages.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-param-reassign */
+import { createSlice } from '@reduxjs/toolkit';
+import { setInitialState } from './channels.js';
+
+const initialState = {
+  messages: [],
+};
+
+const messagesSlice = createSlice({
+  name: 'messagesInfo',
+  initialState,
+  reducers: {
+    addMessage: (state, action) => {
+      state.messages.push(action.payload);
+    },
+    removeChannelMessages: (state, action) => {
+      const newMessages = state.messages
+        .filter(({ channelId }) => channelId !== action.payload.channelId);
+      state.messages = newMessages;
+    },
+  },
+  extraReducers: {
+    [setInitialState.fulfilled]: (state, action) => {
+      state.messages = action.payload.messages;
+    },
+  },
+});
+
+const { reducer, actions } = messagesSlice;
+export const { addMessage, removeChannelMessages } = actions;
+export default reducer;
---
+++ src/slices/modal.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-param-reassign */
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  isOpen: false,
+  type: null,
+  extra: null,
+};
+
+const modalSlice = createSlice({
+  name: 'modal',
+  initialState,
+  reducers: {
+    openModal: (state, action) => {
+      state.isOpen = true;
+      state.type = action.payload.type;
+      state.extra = action.payload.extra;
+    },
+    closeModal: (state) => {
+      state.isOpen = false;
+      state.type = null;
+      state.extra = null;
+    },
+  },
+});
+
+const { reducer, actions } = modalSlice;
+export const { openModal, closeModal } = actions;
+export default reducer;

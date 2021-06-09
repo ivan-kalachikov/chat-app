138bdfa07c19ddd401557faa36c7b3e935a74cf7
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2021-06-09 15:43:00 +0700

    move rollbar token to env varibale and enable only on production

--- src/index.js
+++ src/index.js
@@ -11,14 +11,15 @@ import '../assets/application.scss';
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
-} else {
-// eslint-disable-next-line no-unused-vars
-  const rollbar = new Rollbar({
-    accessToken: '58f78fa326fd4754afcd5e6e7186e9c7',
-    captureUncaught: true,
-    captureUnhandledRejections: true,
-  });
 }
+
+// eslint-disable-next-line no-unused-vars
+const rollbar = new Rollbar({
+  enabled: process.env.NODE_ENV === 'production',
+  accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+});
 
 const init = async () => {
   const socket = io();

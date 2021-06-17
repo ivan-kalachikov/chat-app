218b655ff5782486c4bd7616538192b7dd2a5e08
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2021-06-17 22:09:38 +0700

    refactor initApp

--- src/initApp.jsx
+++ src/initApp.jsx
@@ -3,10 +3,10 @@ import App, { dispatch } from './App.jsx';
 import socketWatcher from './socketWatcher.js';
 import initI18n from './i18n';
 
-const initApp = (socket) => {
-  initI18n();
+const initApp = async (socket) => {
+  await initI18n();
   socketWatcher(socket, dispatch);
-  return Promise.resolve(<App socket={socket} />);
+  return <App socket={socket} />;
 };
 
 export default initApp;

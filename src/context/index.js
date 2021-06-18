956728e1909086624f6fd97a52c6779023c033ae
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2021-06-18 18:19:18 +0700

    refactor context

---
+++ src/context/index.js
@@ -0,0 +1,5 @@
+import AuthTokenContext from './AuthTokenContext.jsx';
+import AuthUsernameContext from './AuthUsernameContext.jsx';
+import SocketInstanceContext from './SocketInstanceContext.jsx';
+
+export { AuthTokenContext, AuthUsernameContext, SocketInstanceContext };
